7cab1f1a86ffdf846fc688b2165f4786e2fa7bc4
mintchkin <mintchkin.dobfner@gmail.com>
2019-01-30 09:10:06 -0500

    Allow perceptron to accept a variable number of inputs

--- understanding_nn/main.py
+++ understanding_nn/main.py
@@ -1,12 +1,16 @@
 import math
 import numpy as np
-import random
+from random import random
 from typing import Callable, List, Tuple
+from itertools import product
 
 
 # Sample training sets
-train_logical_and = [(1, 1, 1), (1, 0, 0), (0, 1, 0), (0, 0, 0)]
-train_logical_or = [(1, 1, 1), (1, 0, 1), (0, 1, 1), (0, 0, 0)]
+train_logical_and = [((1, 1), 1), ((1, 0), 0), ((0, 1), 0), ((0, 0), 0)]
+train_logical_or = [((1, 1), 1), ((1, 0), 1), ((0, 1), 1), ((0, 0), 0)]
+
+train_middle_3 = [(x, x[1]) for x in product((0, 1), (0, 1), (0, 1))]
+train_odd_20 = [(x, sum(x) % 2) for x in product(*((0, 1) for x in range(20)))]
 
 
 class Perceptron:
@@ -14,40 +18,45 @@ class Perceptron:
     A binary perceptron is a type of rudamentary "neural network" with only a single node.
     """
 
-    def __init__(self, threshold: Callable):
+    def __init__(self, inputs: int, threshold: Callable):
         # Initialize random weights for now
-        self.w1 = random.random()
-        self.w2 = random.random()
-        self.wb = random.random()
-
-        self.threshold = threshold
+        self.weights = [random() for x in range(inputs)]
+        self.bias_weight = random()
 
         # Technically this isn't required for a binary perceptron
         self.learning_rate = 0.01
+
+        self.threshold = threshold
         self.bias = 1
 
-    def guess(self, x1: float, x2: float):
+    def guess(self, xs: Tuple[float]):
         """
         Perform a prediction with the current weights.
         """
-        return self.threshold((self.w1 * x1) + (self.w2 * x2) + (self.wb * self.bias))
+        if len(xs) != len(self.weights):
+            raise IndexError(f"Invalid inputs size: {len(xs)} != {len(self.weights)}")
 
-    def learn(self, x1: float, x2: float, expect: float):
+        return self.threshold(
+            sum(x * w for x, w in zip(xs, self.weights)) + self.bias * self.bias_weight
+        )
+
+    def learn(self, xs: Tuple[float], expect: float):
         """
         Correct the weights based on the error
         """
-        error = expect - self.guess(x1, x2)
+        error = expect - self.guess(xs)
 
         # Adjust the weights
-        self.w1 += self.learning_rate * error * x1
-        self.w2 += self.learning_rate * error * x2
-        self.wb += self.learning_rate * error * self.bias
+        for i, x in enumerate(xs):
+            self.weights[i] += self.learning_rate * error * x
+
+        self.bias_weight += self.learning_rate * error * self.bias
 
     def train(self, data: List[Tuple], epochs: int = 3):
         """
         Run the learn function over a collection of training data.
 
-        data = [(x1, x2, expect), ...]
+        data = [((x1, x2, ...), expect), ...]
         """
         print(f"Initial Error: {self.test(data)}")
         for x in range(epochs):
@@ -60,21 +69,18 @@ class Perceptron:
         """
         Return the average error over a list of test data
 
-        data = [(x1, x2, expect), ...]
+        data = [((x1, x2, ...), expect), ...]
         """
-        return sum((expect - self.guess(x1, x2)) for (x1, x2, expect) in data) / len(
-            data
-        )
+        return sum((expect - self.guess(xs)) for (xs, expect) in data) / len(data)
 
     def status(self):
         """
         Convenience function for printing some internal values
         """
         print(
-            f"W1: {self.w1}",
-            f"W2: {self.w2}",
-            f"Wb: {self.wb}",
             f"Activation: {self.threshold.__name__}",
+            f"Wbias: {self.bias_weight}",
+            f"Weights: {self.weights}",
             sep="\n",
         )
 
